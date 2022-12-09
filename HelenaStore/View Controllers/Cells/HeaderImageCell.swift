7221296bcbaf21e35d02b1d6405d894cb48bc5d8
Mindy Douglas <mindy.douglas@icloud.com>
2022-12-08 20:15:42 -0600

    add headerImageCell

---
+++ HelenaStore/View Controllers/Cells/HeaderImageCell.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderImageCell.swift
+//  HelenaStore
+//
+//  Created by Mindy Douglas on 12/8/22.
+//
+
+import UIKit
+
+class HeaderImageCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
